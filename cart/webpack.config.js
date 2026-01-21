fe631c81155e4415e74306019de83975e729d66f
shrilakshmik-pio <Shrilakshmi.GKapase@peerislands.io>
2026-01-21 12:16:54 +0530

    Fix dotenv-webpack failure on Vercel for cart

--- cart/webpack.config.js
+++ cart/webpack.config.js
@@ -1,73 +1,51 @@
+const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { ModuleFederationPlugin } = require("webpack").container;
 const Dotenv = require("dotenv-webpack");
-const { dependencies } = require("./package.json");
 
-module.exports = (env) => {
-  const PRODUCTS_HOST = env.PRODUCTS_HOST || "http://localhost:9002";
-  return {
-    mode: "development",
-    devServer: {
-      port: 9003,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.(js|jsx|ts|tsx)?$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "babel-loader",
-              options: {
-                presets: ["@babel/preset-env", "@babel/preset-react"],
-              },
-            },
-          ],
+module.exports = {
+  entry: "./src/index.tsx",
+  mode: "production",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "auto",
+    clean: true
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: {
+          loader: "ts-loader",
+          options: {
+            transpileOnly: true
+          }
         },
-        {
-          test: /\.(ts|tsx)?$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: "ts-loader",
-            },
-          ],
-        },
-      ],
-    },
-    plugins: [
-      new Dotenv(),
-      new ModuleFederationPlugin({
-        name: "CART",
-        filename: "remoteEntry.js",
-        exposes: {
-          "./Cart": "./src/components/CartButton",
-          "./CheckoutPage": "./src/features/checkout",
-        },
-        remotes: {
-          PRODUCTS: `PRODUCTS@${PRODUCTS_HOST}/remoteEntry.js`,
-        },
-        shared: {
-          ...dependencies,
-          react: {
-            eager: true,
-            singleton: true,
-            requiredVersion: dependencies["react"],
-          },
-          "react-dom": {
-            eager: true,
-            singleton: true,
-            requiredVersion: dependencies["react-dom"],
-          },
-        },
-      }),
-      new HtmlWebpackPlugin({
-        template: "./public/index.html",
-      }),
-    ],
-    resolve: {
-      extensions: [".js", ".jsx", ".ts", ".tsx"],
-    },
-    target: "web",
-  };
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "cart",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./Cart": "./src/Cart"
+      },
+      shared: {
+        react: { singleton: true },
+        "react-dom": { singleton: true }
+      }
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    }),
+    new Dotenv({
+      silent: true   // 🔑 THIS IS THE KEY FIX
+    })
+  ]
 };
+
